d5cdfc2aa61097a8701a21fd9e132e52ea54a539
Misha Claytor <mclaytor@uncc.edu>
2017-03-29 01:55:35 +0000

    Hard coded tag bar, sub tag bar, and added one sample link with an image in the content section

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tags = Tagl.create([{name: 'Breakfast'}, {name: 'Lunch'}, {name: 'Dinner'}, {name: 'Dessert'}])
