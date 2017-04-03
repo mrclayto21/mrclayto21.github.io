fb10983eb56c1fdf61005e14e67367bc84a73c09
Misha Claytor <mclaytor@uncc.edu>
2017-04-03 15:55:35 +0000

    Updated the recipe_controller

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,11 +1,43 @@
 class RecipeController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+  
   def add
+    @recipe = Recipe.new(recipe_params)
+    byebug
+    
+    if @recipe.save
+      byebug
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
   end
   
   def update
+    @recipe = Recipe.find(params[:id])
+    
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
   end
   
   def delete
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    
+    redirect_to recipe_path
   end
-  
 end
+
+private
+    def article_params
+      params.require(:recipe).permit(:title, :text)
+    end
