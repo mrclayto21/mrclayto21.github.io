91f254533e1f2d4bc38944b2040499400b34744b
Misha Claytor <mclaytor@uncc.edu>
2017-04-04 00:36:15 +0000

    Added fb share feature

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,44 +1,20 @@
 class RecipeController < ApplicationController
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
   end
   
-  def add
+  def create
     @recipe = Recipe.new(recipe_params)
-    byebug
-    
     if @recipe.save
-      byebug
-      redirect_to @recipe
-    else
+      redirect_to root_path
+    else 
       render 'new'
     end
   end
-  
-  def edit
-    @recipe = Recipe.find(params[:id])
-  end
-  
-  def update
-    @recipe = Recipe.find(params[:id])
-    
-    if @recipe.update(recipe_params)
-      redirect_to @recipe
-    else
-      render 'edit'
-    end
-  end
-  
-  def delete
-    @recipe = Recipe.find(params[:id])
-    @recipe.delete
-    
-    redirect_to recipe_path
-  end
-end
 
 private
     def recipe_params
-      params.require(:recipe).permit(:title, :text)
+      params.require(:recipe).permit(:title, :instruction, :tag_id, ingredients_attributes: [:name])
     end
 end
